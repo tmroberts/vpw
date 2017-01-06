ee9c01a9eb878ac3e819b1067b969abbc9ebf359
Tim Roberts <tmroberts7@gmail.com>
2017-01-05 21:59:14 -0600

    Added jquery to handle 'displaying navigation on-scroll' feature to each page

--- public/script.js
+++ public/script.js
@@ -1,22 +1,34 @@
 //alert('I am working! !');
 console.log('This is working');
 
+
+
 'use strict';
 if (this.ClickTimes === undefined) this.ClickTimes = {};
 
 (function(context) {
-
-  var clickTimes = [];
-  var clickLogUlDOM;
+  
+  (function($) {
+      $(document).ready(function(){
+          $(window).scroll(function(){
+              if ($(this).scrollTop() > 100) {
+                  $('nav').fadeIn(500);
+              } else {
+                  $('nav').fadeOut(500);
+              }
+          });
+      });
+  })(jQuery);
 
 
   function start() {
 
     //Call your code here
     console.log('starting!', context);
+    var clickTimes = [];
+    var clickLogUlDOM;
 
 
-    ;
 
   }
 
